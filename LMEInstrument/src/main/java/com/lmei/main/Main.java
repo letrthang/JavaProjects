fed54e23f41d18fc9bae1929d78b7103d2144297
Thang Le-Surface 4 <letrthang@hotmail.com>
2017-11-24 17:26:23 +0800

    1st time checkin

---
+++ LMEInstrument/src/main/java/com/lmei/main/Main.java
@@ -0,0 +1,19 @@
+package com.lmei.main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+	/** logger. */
+	private static final Logger LOG = LogManager.getLogger(Main.class);
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		LOG.debug("hello world!");
+
+	}
+
+}
