c0645a8224fb4ba25e8d2294fb8663a451df77cc
Thang Le <letrthang@gmail.com>
2019-06-06 16:15:46 +0700

    draw

--- Drawing_On_Console/src/Main.java
+++ Drawing_On_Console/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author Thang Le
@@ -8,29 +11,84 @@
  *
  */
 public class Main {
-	static Pixel[][] screen;
+	static Pixel[][] screenData;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		// Enter data using BufferReader
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		// Reading data using readLine
+		try {
+			System.out.println("start draw...");
+
+			for (;;) {
+				String input = reader.readLine();
+
+				String command = input.substring(0, 1);
+				if (command.equals("C")) {
+
+					// this command is only 2 arguments
+					String[] arguments = input.split("\\s+");
+					// 1. draw canvas (width_X, height_Y)
+					drawCanvas(Integer.parseInt(arguments[1]), Integer.parseInt(arguments[2]));
+
+				} else if (command.equals("L")) {
+
+					// this command is only 4 arguments
+					String[] arguments = input.split("\\s+");
+					// 2. draw line
+					drawLine(Integer.parseInt(arguments[1]), Integer.parseInt(arguments[2]),
+							Integer.parseInt(arguments[3]), Integer.parseInt(arguments[4]));
+
+				} else if (command.equals("R")) {
+
+					// this command is only 4 arguments
+					String[] arguments = input.split("\\s+");
+					// 3. draw rectangle
+					drawRectangle(Integer.parseInt(arguments[1]), Integer.parseInt(arguments[2]),
+							Integer.parseInt(arguments[3]), Integer.parseInt(arguments[4]));
+
+				} else if (command.equals("B")) {
+					// this command is only 3 arguments
+					String[] arguments = input.split("\\s+");
+					// 4. fill in bucket with color
+					bucketFillConsole(Integer.parseInt(arguments[1]), Integer.parseInt(arguments[2]),
+							arguments[3].charAt(0));
+
+				} else if (command.equals("Q")) {
+					System.out.println("quit program ...");
+					break;
+				} else {
+					System.out.println("invalid input...");
+					break;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		// Test code
 		// we just do a simple test code here and see results in console.
 		// It is not simple to write unit test here because outputs to draw data to
 		// console.
 
 		// 1. draw canvas (width_X, height_Y)
-		drawCanvas(25, 10);
+		// drawCanvas(25, 10);
 		// 2. draw vertical line
-		drawLine(2, 3, 2, 8);
+		// drawLine(2, 3, 2, 8);
 		// 3. draw horizon line
-		drawLine(2, 8, 8, 8);
+		// drawLine(2, 8, 8, 8);
 		// 4. draw rectangle
-		drawRectangle(10, 2, 20, 6);
+		// drawRectangle(10, 2, 20, 6);
 		// 5. fill in bucket with color 'c'
-		bucketFillConsole(8, 2, 'c');
+		// bucketFillConsole(8, 2, 'c');
 		// 6. fill in bucket with color '@'
-		bucketFillConsole(12, 3, '@');
+		// bucketFillConsole(12, 3, '@');
 
 	}
 
@@ -41,15 +99,15 @@ public class Main {
 	 * @param height (Y)
 	 */
 	static void drawCanvas(int width, int height) {
-		screen = new Pixel[height][width];
+		screenData = new Pixel[height][width];
 
 		// init all pixels of screen
 		for (int n = 0; n < height; n++) {
 			for (int m = 0; m < width; m++) {
-				screen[n][m] = new Pixel();
-				screen[n][m].setX(m);
-				screen[n][m].setY(n);
-				screen[n][m].setText(' ');
+				screenData[n][m] = new Pixel();
+				screenData[n][m].setX(m);
+				screenData[n][m].setY(n);
+				screenData[n][m].setText(' ');
 			}
 		}
 
@@ -58,14 +116,15 @@ public class Main {
 		for (int n = 0; n < height; n++) {
 			for (int m = 0; m < width; m++) {
 				if (n == 0 || n == height - 1) {
-					screen[n][m].setText('-');
+					screenData[n][m].setText('-');
 				} else if (m == 0 || m == width - 1) {
-					screen[n][m].setText('|');
+					screenData[n][m].setText('|');
 				}
 			}
 		}
 
-		System.out.println("Draw canvas: " + "width (X):" + screen[0].length + ", height (Y): " + screen.length);
+		System.out
+				.println("Draw canvas: " + "width (X):" + screenData[0].length + ", height (Y): " + screenData.length);
 
 		// display Pixels to console
 		draw2Screen();
@@ -82,18 +141,19 @@ public class Main {
 	 */
 	static void drawLine(int x1, int y1, int x2, int y2) {
 		// 1. do basic input validation
-		if (x1 > screen[0].length || x2 > screen[0].length || y1 > screen.length || y2 > screen.length) {
+		if (x1 > screenData[0].length || x2 > screenData[0].length || y1 > screenData.length
+				|| y2 > screenData.length) {
 			System.out.println("drawLine: points are out of canvas");
 			return;
 		}
 
 		if (x1 == x2) {
 			for (int i = y1; i <= y2; i++) {
-				screen[i][x1].setText('x');
+				screenData[i][x1].setText('x');
 			}
 		} else if (y1 == y2) {
 			for (int i = x1; i <= x2; i++) {
-				screen[y1][i].setText('x');
+				screenData[y1][i].setText('x');
 			}
 		} else {
 
@@ -153,14 +213,16 @@ public class Main {
 		// using recursive algorithm to fill bucket.
 		// Note: coordinate (x,y) <=> array[y][x]
 
-		if (x > screen[0].length || y > screen.length) {
+		if (x > screenData[0].length || y > screenData.length) {
 			System.out.println("Fill bucket: point is out of canvas");
 			return;
 		}
 
-		if (screen[y][x].getText() == ' ') {
+		// fill color at coordinate [x,y] and its left, right, top and bottom.
+		// until reaching border.
+		if (screenData[y][x].getText() == ' ') {
 
-			screen[y][x].setText(color);
+			screenData[y][x].setText(color);
 
 			bucketFill(x + 1, y, color);
 			bucketFill(x - 1, y, color);
@@ -175,12 +237,12 @@ public class Main {
 	 */
 	static void draw2Screen() {
 
-		for (int n = 0; n < screen.length; n++) {
+		for (int n = 0; n < screenData.length; n++) {
 
-			for (int m = 0; m < screen[0].length; m++) {
+			for (int m = 0; m < screenData[0].length; m++) {
 
-				if (screen[n][m].getText() != 0) {
-					System.out.print(screen[n][m].getText());
+				if (screenData[n][m].getText() != 0) {
+					System.out.print(screenData[n][m].getText());
 				} else {
 					System.out.print(" ");
 				}
